8759999d8aa5c8dd8126ad10d978a6705fcc25d8
Kyla Rieger <kyla.styles@gmail.com>
2017-12-19 22:55:53 -0500

    added numerical validation

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ form = """
     <html>
         <head>
             <style>
+            error {{
+                color: red;
+            }}
+
             form {{
                 background-color: #eee;
                 padding: 20px;
@@ -35,17 +39,36 @@ form = """
     </html>
 """
 
+#check that rot entry is a valid number
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
 
+#show form on index page
 @app.route("/")
 def index():
     return form.format("")
 
+#respond to user entry
 @app.route("/", methods=["post", "get"])
 def encrypt():
-    rot = int(request.form["rot"])
+
+    #set variables
+    rot = request.form["rot"]
     text = request.form["text"]
-   
-    rotated = rotate_string(text,rot)
-    return form.format(rotated)
+
+    #check for valid numerical entry
+    if is_integer(rot) == True:
+        rot = int(rot)
+        rotated = rotate_string(text, rot)
+        #return encrypted text if all good
+        return form.format(rotated)
+    else:
+        rot_error = "Please enter whole numbers only."
+        #return error message and blank form if bad
+        return form.format("") + rot_error
 
 app.run()
